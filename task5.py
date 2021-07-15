060ea8753213c19b422b91c0158f28a2136089bd
wooihaw <wooihaw.tan@gmail.com>
2021-07-15 17:14:24 +0800

    gitautosync update 14

---
+++ task5.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 15 17:12:20 2021
+
+@author: wooihaw
+"""
+# Task 5
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('https://raw.githubusercontent.com/wooihaw/datasets/main/covid19_malaysia.csv')
+print(df.sample(10))

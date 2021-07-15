efdefc0225e322f919b2c8e5bde1b4b5c4154752
wooihaw <wooihaw.tan@gmail.com>
2021-07-15 16:46:42 +0800

    gitautosync update 3

--- task2.py
+++ task2.py
@@ -4,7 +4,15 @@ Created on Thu Jul 15 16:42:45 2021
 
 @author: wooihaw
 """
+# Task 2
 alist = ['Python', 'Java', 'Perl', 'PHP', 'JavaScript', 'C++', 'C#', 'Ruby', 'R']
 
 results = [i for i in alist if i[0] == 'P']
 print(results)
+
+# alternative solutions
+res2 = []
+for i in alist:
+    if i[0] == 'P':
+        res2.append(i)
+print(res2)

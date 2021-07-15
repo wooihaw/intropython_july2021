a6bb504a2ce427748cca2a3ec733c6b42d9ddd5c
wooihaw <wooihaw.tan@gmail.com>
2021-07-15 16:45:16 +0800

    gitautosync update 2

---
+++ task2.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 15 16:42:45 2021
+
+@author: wooihaw
+"""
+alist = ['Python', 'Java', 'Perl', 'PHP', 'JavaScript', 'C++', 'C#', 'Ruby', 'R']
+
+results = [i for i in alist if i[0] == 'P']
+print(results)

e0e4791e0197dd0145cf30f570681f83e76eaa21
wooihaw <wooihaw.tan@gmail.com>
2021-07-15 17:01:36 +0800

    gitautosync update 10

---
+++ task4.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 15 16:58:41 2021
+
+@author: wooihaw
+"""
+# Task 4
+from collections import Counter
+
+with open('alice.txt', 'r') as f:
+    s = f.read()
+    
+print(s)
